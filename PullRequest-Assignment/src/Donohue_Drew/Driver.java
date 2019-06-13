a9fe141666933a03b54de8abaf268529c5a1d738
Drew Donohue <Drewdonohue13@gmail.com>
2019-06-13 13:59:02 -0400

    I finished the work from today.
    I finished the whole program and had a seperate method calling the three different methods. I checked all of the answers and they work.

---
+++ PullRequest-Assignment/src/Donohue_Drew/Driver.java
@@ -0,0 +1,14 @@
+package Donohue_Drew;
+
+public class Driver 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Sum of all the multiples of 3 or 5 below 1000: " + Exercises.threeAndFive());
+		System.out.println("Sum of the even-valued terms less than 4 million in the Fibonacci Sequence:" + Exercises.evenFibSum());
+		System.out.println("The largest prime factor of the number 600851475143: " + Exercises.largestPrimeFactor());
+
+	}
+
+}
