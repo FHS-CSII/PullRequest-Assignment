4eb75cf99705e769d3f46cea09a1409391929966
abbeycowles12 <51758267+abbeycowles12@users.noreply.github.com>
2019-06-13 13:59:13 -0400

    i did a bunch of methods w numbers

---
+++ PullRequest-Assignment/src/Cowles_Abigail/run.java
@@ -0,0 +1,14 @@
+package Cowles_Abigail;
+
+public class run {
+
+	public static void main(String[] args) {
+		Excercises e = new Excercises();
+		
+		System.out.println(e.threeAndFive());
+		System.out.println(e.evenFibSum());
+		System.out.println(e.largestPrimeFactor());
+
+	}
+
+}
