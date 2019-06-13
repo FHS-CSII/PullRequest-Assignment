779d8971274b2b0148bacc1e00460e66985ab8e9
ryanpuharic <44206042+ryanpuharic@users.noreply.github.com>
2019-06-13 14:01:56 -0400

    Summary
    Program that does various mathematical operation and prints out their results.

---
+++ PullRequest-Assignment/src/Puharic_Ryan/Driver.java
@@ -0,0 +1,14 @@
+/**
+ * @author Ryan Puharic
+ **/
+package Puharic_Ryan;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		System.out.println(Exercises.threeAndFive());
+		System.out.println(Exercises.evenFibSum());
+		System.out.println(Exercises.largestPrimeFactor());
+	}
+
+}
