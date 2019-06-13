f3a68c9ded9c7837f6eb8a926b182de3867b1653
Jason Wang <34901923+121jwang@users.noreply.github.com>
2019-06-13 13:30:45 -0400

    Added the driver
    Actually followed instructions this time

---
+++ PullRequest-Assignment/src/wang_jason/Driver.java
@@ -0,0 +1,10 @@
+package wang_jason;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		System.out.println(Exercises.threeAndFive());
+		System.out.println(Exercises.evenFibSum());
+		System.out.println(Exercises.largestPrimeFactor());
+	}
+}
