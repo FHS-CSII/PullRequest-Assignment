d6380c3563e9b6030206da073510ddb8982a646a
mhon2 <43385616+mhon2@users.noreply.github.com>
2019-06-17 12:47:53 -0400

    finally finished
    finished because i forgot to commit changes last week

---
+++ PullRequest-Assignment/src/Hon_Michelle/Driver.java
@@ -0,0 +1,11 @@
+package Hon_Michelle;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		System.out.println(Exercises.threeAndFive());
+		System.out.println(Exercises.evenFibSum());
+		System.out.println(Exercises.largestPrimeFactor()); 
+	}
+
+}
